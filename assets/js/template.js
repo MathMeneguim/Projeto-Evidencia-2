b9211efad1c0e6f9ea2a8584b23b26aa0f94fdb7
Matheus Meneguim <matheusdelfino@almavivasolutions.com.br>
2024-12-26 09:58:47 -0300

    feat: Subindo o protótipo do navbar, protótipo da section category, imagens, arquivos bootstrap e css's

---
+++ assets/js/template.js
@@ -0,0 +1,19 @@
+fetch('./assets/html/nav.html')
+	.then(res => res.text())
+	.then(data => document.getElementById('nav-template').innerHTML = data)
+	.catch(console.error);
+	
+fetch('../html/home.html')
+.then(res => res.text())
+.then(data => document.getElementById('home-template').innerHTML = data)
+.catch(console.error);
+
+fetch('../assets/html/category.html')
+.then(res => res.text())
+.then(data => document.getElementById('category-template').innerHTML = data)
+.catch(console.error);
+
+fetch('../assets/html/trending.html')
+	.then(res => res.text())
+	.then(data => document.getElementById('trending-template').innerHTML = data)
+	.catch(console.error);
