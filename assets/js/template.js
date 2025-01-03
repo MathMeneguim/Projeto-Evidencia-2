76a659bd2ce7224af104d2a2d29ac3ee5629fc61
Matheus Meneguim <matheusdelfino@almavivasolutions.com.br>
2025-01-03 08:47:26 -0300

    feat: protótipo da seção trending

--- assets/js/template.js
+++ assets/js/template.js
@@ -3,15 +3,15 @@ fetch('./assets/html/nav.html')
 	.then(data => document.getElementById('nav-template').innerHTML = data)
 	.catch(console.error);
 	
-fetch('../html/home.html')
-.then(res => res.text())
-.then(data => document.getElementById('home-template').innerHTML = data)
-.catch(console.error);
+// fetch('../html/home.html')
+// .then(res => res.text())
+// .then(data => document.getElementById('home-template').innerHTML = data)
+// .catch(console.error);
 
-fetch('../assets/html/category.html')
-.then(res => res.text())
-.then(data => document.getElementById('category-template').innerHTML = data)
-.catch(console.error);
+// fetch('../assets/html/category.html')
+// .then(res => res.text())
+// .then(data => document.getElementById('category-template').innerHTML = data)
+// .catch(console.error);
 
 fetch('../assets/html/trending.html')
 	.then(res => res.text())
