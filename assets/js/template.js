13f2db5b1a518447cce727b26276a0335a844028
Matheus Meneguim <matheusdelfino@almavivasolutions.com.br>
2025-01-06 17:12:20 -0300

    feat: adicionadas novas imagens, adicionadas fontes; fix: aperfeiçoamento das sections

--- assets/js/template.js
+++ assets/js/template.js
@@ -2,11 +2,6 @@ fetch('./assets/html/nav.html')
 	.then(res => res.text())
 	.then(data => document.getElementById('nav-template').innerHTML = data)
 	.catch(console.error);
-	
-// fetch('../html/home.html')
-// .then(res => res.text())
-// .then(data => document.getElementById('home-template').innerHTML = data)
-// .catch(console.error);
 
 fetch('../assets/html/category.html')
 .then(res => res.text())
